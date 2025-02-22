c98d8c55d415f27fbecbeef864ffc6a4cf88c116
Noah Surprenant <noah.surprenant@gmail.com>
2025-02-21 23:55:36 -0500

    Fixup grid spacing

--- JobBot.Client/src/app/questions/questions.component.ts
+++ JobBot.Client/src/app/questions/questions.component.ts
@@ -7,10 +7,12 @@ import { InputComponent } from '../input/input.component';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { QuestionBase, QuestionControlService } from '../questionControl.service';
 import { DropdownComponent } from '../dropdown/dropdown.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-questions',
   imports: [
+    CommonModule,
     ButtonComponent,
     InputComponent,
     DropdownComponent,
@@ -27,6 +29,14 @@ export class QuestionsComponent {
   form!: FormGroup;
 
   constructor(private http: HttpClient, private toastService: ToastService, private qcs: QuestionControlService) {}
+
+  isEven(i: number) {
+    return i % 2 == 0;
+  }
+
+  isOdd(i: number) {
+    return !this.isEven(i);
+  }
 
   clicked(): void {
     this.loading.set(true);
